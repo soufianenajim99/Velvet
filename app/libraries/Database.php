322fc76e0fb46234798b2ddbe499c7cacec78f76
SoufianeNajim <soufianenajim100@gmail.com>
2024-01-01 16:16:32 +0100

    Working on Services lib

---
+++ app/libraries/Database.php
@@ -0,0 +1,18 @@
+<?php
+   class Database {
+    private $dbname = CONFIG['db'];
+    private $user = CONFIG['db_user'];
+    private $pass = CONFIG['db_password'];
+
+    private $dbh;
+
+    public function __construct(){
+    try{
+      $this->dbh = new PDO($this->dbname, $this->user, $this->pass);
+     }catch(PDOException $e){
+    echo $e->getMessage();
+     }
+    }
+
+
+  }
