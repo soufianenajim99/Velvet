76c350626db1ed5a3747e170209693bd8adcd1bd
Agourd06 <oualidagourd@gmail.com>
2024-01-02 10:24:50 +0100

    product list page / basket

---
+++ app/views/client/Basket.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>Document</title>
+</head>
+
+<body>
+  
+
+    <body>
+        <div class="min-h-screen bg-gray-100 pt-20">
+            <h1 class="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+            <div class=" mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+                <div class="rounded-lg md:w-2/3">
+                <a class="justify-between mb-6 rounded-lg w-[100%] hover:gap-[20px] bg-white p-6 shadow-md sm:flex sm:justify-start" href="product.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                            <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+                        </svg>
+                        <p class="sm:ml-4 sm:flex sm:w-full sm:justify-between">Product List</p>
+
+
+                    </a>
+                    <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                        <img src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="product-image" class="w-full rounded-lg sm:w-40" />
+                        <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                            <div class="mt-5 sm:mt-0">
+                                <h2 class="text-lg font-bold text-gray-900">Nike Air Max 2019</h2>
+                                <p class="mt-1 text-xs text-gray-700">36EU - 4US</p>
+                            </div>
+                            <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                                <select name="Quantity" value="Quantity" id="" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+                                    <option value="">1</option>
+                                    <option value="">2</option>
+                                    <option value="">3</option>
+                                    <option value="">4</option>
+                                    <option value="">5</option>
+                                    <option value="">6</option>
+                                    <option value="">7</option>
+                                    <option value="">8</option>
+                                    <option value="">9</option>
+
+                                </select>
+                                <div class="flex items-center space-x-4">
+                                    <p class="text-sm">259.000 ₭</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  
+        
+              
+         
+                  
+                   
+                    
+                </div>
+                <!-- Sub total -->
+                <div class=" mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+                    <div class="mb-2 flex justify-between">
+                        <p class="text-gray-700">Subtotal</p>
+                        <p class="text-gray-700">$129.99</p>
+                    </div>
+                    <div class="flex justify-between">
+                        <p class="text-gray-700">Shipping</p>
+                        <p class="text-gray-700">$4.99</p>
+                    </div>
+                    <hr class="my-4" />
+                    <div class="flex justify-between">
+                        <p class="text-lg font-bold">Total</p>
+                        <div class="">
+                            <p class="mb-1 text-lg font-bold">$134.98 USD</p>
+                            <p class="text-sm text-gray-700">including VAT</p>
+                        </div>
+                    </div>
+                    <button class="mt-6 w-full rounded-md bg-black py-1.5 font-medium text-gray-600 hover:bg-gray-600">Check out</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</body>
+
+</html>
