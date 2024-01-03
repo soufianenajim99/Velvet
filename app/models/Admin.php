a2549bd317ff5d4f52562e6057c3ab4c1a9db1f2
SoufianeNajim <soufianenajim100@gmail.com>
2024-01-03 10:08:52 +0100

    models updated

--- app/models/Admin.php
+++ app/models/Admin.php
@@ -1,12 +1,8 @@
 <?php
 
-class Admin{
-    private $Id_admin;
-    private $Id_user;
-
-    public function __construct($Id_admin, $Id_user){
-        $this->Id_admin = $Id_admin;
-        $this->Id_user = $Id_user;
+class Admin extends Users{
+    public function __construct($id, $FullName, $username, $Password, $Adresse, $Email){
+        parent::__construct($id,$FullName, $username, $Password, $Adresse, $Email);
     }
     public function __get($property) {
         if (property_exists($this, $property)) {
