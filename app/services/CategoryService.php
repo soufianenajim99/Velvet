493ec521bedf8154c17e67756ba0450c12dacd72
Agourd06 <oualidagourd@gmail.com>
2024-01-03 14:42:37 +0100

    singleton pattern

--- app/services/CategoryService.php
+++ app/services/CategoryService.php
@@ -1,1 +1,28 @@
-<
+<?php
+
+
+
+class CategoryService implements ImCategoryService{
+
+  private $db;
+  private $conn;
+  public function __construct(){
+
+
+$this->db = Database::getInstance();
+$this->conn = $this->db->getConnection();
+
+
+}
+public function displayCategory(){
+    $conn = $this->conn;
+    
+}
+
+
+
+
+
+}
+
+?>
