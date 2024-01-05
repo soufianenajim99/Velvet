d17e2344cbef7034c9d543c8986ade56e13c8192
abdrahmane-03 <abdrahmanhafidi03@gmail.com>
2024-01-05 12:32:11 +0100

    admin dash

--- app/services/ProductInterface.php
+++ app/services/ProductInterface.php
@@ -6,6 +6,8 @@ interface ProductInterface {
     public function addproduct(product $product);
     public function deleteproduct($productId);
     public function updateproduct(product $updatedproduct);
+    
+    public function countproducts();
 
 }
 
